6c84dd4d7e286bd5e98d98700dbe54e0a1c8241e
samialsohan <samialsohan@gmail.com>
2026-03-16 22:31:19 +0000

    printing session id

--- src/main/java/com/springauth/auth/HelloController.java
+++ src/main/java/com/springauth/auth/HelloController.java
@@ -1,13 +1,19 @@
 package com.springauth.auth;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping
-    public String greet(){
-        return "Hello World!";
+    @GetMapping("hello")
+    public String greet(HttpServletRequest req){
+        return "Hello your session id: " + req.getSession().getId();
+    }
+    @GetMapping("about")
+    public String about()
+    {
+        return "I am Sohan";
     }
 }
