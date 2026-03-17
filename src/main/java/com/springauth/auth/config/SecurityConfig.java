7cf8f53d2d55f5ba38bccebd2082ccbf9533b8a9
samialsohan <samialsohan@gmail.com>
2026-03-17 21:05:02 +0000

    stateless session creation, every response with  new session

--- src/main/java/com/springauth/auth/config/SecurityConfig.java
+++ src/main/java/com/springauth/auth/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +15,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(customizer->customizer.disable());
         http.authorizeHttpRequests(request ->request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
+        //http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
